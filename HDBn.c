8b4bc3357cf055a013b3e5af47f7c7192441407a
NomeUn <48085180+NomeUn@users.noreply.github.com>
2020-01-05 20:09:23 +0100

    Update HDBn.c

--- HDBn.c
+++ HDBn.c
@@ -10,24 +10,20 @@ void hdbn(int x, int signal[x], int N[x], int P[x], int n){
                 if(viol == -1){
                     viol = 1;
                     if(last == -1){
-                        printf("viol P 101\n");
                         last = 1;
                         P[i] = 1;
                         P[i-n] = 1;
                     }
                     else{
-                        printf("viol P 001\n");
                         P[i] = 1;
                     }
                 }
                 else{
                     viol = -1;
                     if(last == -1){
-                        printf("viol N 001\n");
                         N[i] = 1;
                     }
                     else{
-                        printf("viol N 101\n");
                         last = -1;
                         N[i] = 1;
                         N[i-n] = 1;
@@ -36,7 +32,6 @@ void hdbn(int x, int signal[x], int N[x], int P[x], int n){
                 nb = 0;
             }
             else{
-                printf("0\n");
             }
         }
         else{
@@ -45,12 +40,10 @@ void hdbn(int x, int signal[x], int N[x], int P[x], int n){
             if(last == -1){
                 P[i] = 1;
                 last = 1;
-                printf("P1\n");
             }
             else{
                 N[i] = 1;
                 last = -1;
-                printf("N1\n");
             }
             
         }
@@ -61,16 +54,112 @@ void hdbn(int x, int signal[x], int N[x], int P[x], int n){
 }
 
 void decode(int x, int N[x], int P[x], int res[x], int n, int erreur[x]){
-    for (int i = 0; i < x; i++){
-        if(N[i] == 0 && P[i] == 0){
-            res[i] = 0;
-        }
-        else if(N[i] == 1 && P[i] == 1){
-            erreur[i] = 1;
+    int indice = x, viol, j;
+    for (int i = x-1; i >= 0; i--){
+        if(indice > i){
+            if(N[i] == 0 && P[i] == 0){
+                res[i] = 0;
+            }
+            else if(N[i] == 1 && P[i] == 1){
+                erreur[i] = 1;
+            }
+            else if(N[i] == 1){
+                if(i > 0 && N[i-1] == 1){
+                    erreur[i] = 1;
+                }
+                else if (N[i] == 1 && i < n){
+                    res[i] = 1;
+                }
+                else{
+                    if (N[i-n] == 1){
+                        viol = 1;
+                        for (j = i-1; j > i-n && j >= 0 && viol == 1; j--){
+                            if(P[j] == 1){
+                                viol = 0;
+                                res[i] = 1;
+                            }
+                            if(N[j] == 1){
+                                erreur[j] = 1;
+                            }
+                        }
+                        if(viol){
+                            indice = j;
+                        }
+                    }
+                    else if(N[i-n-1] == 1){
+                        viol = 1;
+                        for (j = i-1; j > i-n-1 && j >= 0 && viol == 1; j--){
+                            if(P[j] == 1){
+                                viol = 0;
+                                res[i] = 1;
+                            }
+                            if(N[j] == 1){
+                                erreur[j] = 1;
+                            }
+                            
+                        }
+                        if(viol){
+                            indice = j+1;
+                        }
+                    }
+                    else{
+                        res[i] = 1;
+                    }                 
+                    
+                }
+                
+                
+            }
+            else{
+                if(i > 0 && P[i-1] == 1){
+                    erreur[i] = 1;
+                }
+                else if (P[i] == 1 && i < n){
+                    res[i] = 1;
+                }
+                else{
+                    if (P[i-n] == 1){
+                        viol = 1;
+                        for (j = i-1; j > i-n && j >= 0 && viol == 1; j--){
+                            if(N[j] == 1){
+                                viol = 0;
+                                res[i] = 1;
+                            }
+                            if(P[j] == 1){
+                                erreur[j] = 1;
+                            }
+                        }
+                        if(viol){
+                            indice = j;
+                        }
+                    }
+                    else if(P[i-n-1] == 1){
+                        viol = 1;
+                        for (j = i-1; j > i-n-1 && j >= 0 && viol == 1; j--){
+                            if(N[j] == 1){
+                                viol = 0;
+                                res[i] = 1;
+                            }
+                            if(P[j] == 1){
+                                erreur[j] = 1;
+                            }
+                        }
+                        if(viol){
+                            indice = j+1;
+                        }
+                    }
+                    else{
+                        res[i] = 1;
+                    }
+                    
+                    
+                }
+                
+                
+            }
         }
         
     }
-    
 }
 
 
@@ -78,15 +167,35 @@ int main(){
     int x = 23;//taille signal
     int signal[] = {1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0}, N[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, P[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int n = 2;//HDBn
+    printf("signal : ");
+    for (int i = 0; i < x; i++){
+        printf("%d", signal[i]);
+    }
     hdbn(x, signal, N, P, n);
+    printf("\n\nP : ");
     for (int i = 0; i < x; i++){
-        printf("%d",P[i]);
+        printf("%d", P[i]);
     }
-    printf("\n");
+    printf("\nN : ");
     for (int i = 0; i < x; i++){
-        printf("%d",N[i]);
+        printf("%d", N[i]);
     }
-    printf("\n");
+    printf("\n\n");
+    int res[23], erreur[23];
+    for (int i = 0; i < x; i++){
+        res[i] = 0;
+        erreur[i] = 0;
+    }
+    decode(x, N, P, res, n, erreur);
+
+    printf("Resultat : ");
+    for (int i = 0; i < x; i++){
+        printf("%d", res[i]);
+    }
+    printf("\nErreur : ");
+    for (int i = 0; i < x; i++){
+        printf("%d", erreur[i]);
+    }
 
 }
 
